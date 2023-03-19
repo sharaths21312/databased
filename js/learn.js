8a016d51a22b91cbdda0d62fa67ba44124f07ccd
YUKTA-S <yukta.subramanian@gmail.com>
2023-03-19 18:51:18 +0530

    learn2

--- js/learn.js
+++ js/learn.js
@@ -1,97 +1,49 @@
-document.querySelector("#nav-hamburger").addEventListener("click", function () {
-    if (document.querySelector("#nav-hamburger").getAttribute("data-state") == "inactive") {
-        openMenu();
-    }
-    else {
-        closeMenu();
-    }
-});
+var difficulty_filter = ['hard', 'medium', 'easy'];
+var topic_filter = ['Arrays', 'Greedy'];
 
-document.querySelectorAll("#mobileMenu a").forEach(function (e) {
-    e.addEventListener("click", function () {
-        closeMenu();
+document.querySelectorAll(".filter button").forEach(function (btn) {
+
+    btn.addEventListener("click", function (event) {
+        let type = this.parentElement.getAttribute('data-type');
+
+        if (this.classList.contains("active")) {
+            this.classList.remove("active");
+            window[type + "_filter"].splice(window[type + "_filter"].indexOf(this.getAttribute('data')), 1)
+
+        } else {
+            this.classList.add("active");
+            window[type + "_filter"].push(this.getAttribute('data'));
+        }
     });
 });
 
-function closeMenu() {
-    document.querySelector("#nav-hamburger").setAttribute("data-state", "inactive");
-    document.querySelector("#mobileMenu").classList.add("fadeout");
-    setTimeout(() => {
-        document.querySelector("#mobileMenu").style.display = "none";
-        document.querySelector("#mobileMenu").classList.remove("fadeout");
-    }, 300);
-    document.querySelectorAll("#nav-hamburger span").forEach(function (e) {
-        e.style.display = "block";
+fetch(
+    `https://api.airtable.com/v0/appHwUzo4ARCQQlwr/Learn?maxRecords=1000&view=Grid%20view`,
+    {
+        method: 'GET',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            Authorization: `Bearer pat2bEq3dsaXHSBH9.2edd33a7b1c2de8fd5e4fe14b82900cf807d2c9b56dfead6a8bdd48715826409`,
+        },
+    }
+).then(response => {
+    return response.json();
+})
+.then(response => {
+    response.records.forEach(function(row){
+        let topicsString = "";
+        row.fields.Topics.forEach(function(topic){
+            topicsString += `<span class="topicTag">${topic}</span>`;
+        });
+        document.querySelector("#container").innerHTML += `<div class="card">
+            <h2>${row.fields.Name}</h2>
+            <span class="difficultyTag ${row.fields.Difficulty}">${row.fields.Difficulty}</span>
+            <span class="topicTag">${topicsString}</span>
+            <a href="${row.fields.Link}" target="_blank">Open Problem</a>
+        </div>`;
     });
-    document.querySelector("#nav-hamburger p").style.display = "none";
-}
-function openMenu() {
-    document.querySelector("#nav-hamburger").setAttribute("data-state", "active");
-    document.querySelector("#mobileMenu").style.display = "block";
-    document.querySelectorAll("#nav-hamburger span").forEach(function (e) {
-        e.style.display = "none";
-    });
-    document.querySelector("#nav-hamburger p").style.display = "block";
-}
-
-
-function setBgColor() {
-    var percent = (document.documentElement.scrollTop / (document.documentElement.scrollHeight - document.documentElement.clientHeight));
-    document.querySelector("body").style.background = `linear-gradient(
-      #180834 0,
-      #26032c calc(100vh + 0px),
-      rgb(${16 + percent * (15 - 16)}, ${16 + percent * (84 - 16)}, ${16 + percent * (138 - 16)}) 100vh
-    )`;
-}
-
-window.addEventListener("scroll", function () {
-    setBgColor();
 });
-filterSelection("all")
-function filterSelection(c) {
-  var x, i;
-  x = document.getElementsByClassName("filterDiv");
-  if (c == "all") c = "";
-  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
-  for (i = 0; i < x.length; i++) {
-    w3RemoveClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
-  }
-}
+window.addEventListener('resize', function(event) {
+}, true);
 
-// Show filtered elements
-function w3AddClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) == -1) {
-      element.className += " " + arr2[i];
-    }
-  }
-}
-
-// Hide elements that are not selected
-function w3RemoveClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1);
-    }
-  }
-  element.className = arr1.join(" ");
-}
-
-// Add active class to the current control button (highlight it)
-var btnContainer = document.getElementById("myBtnContainer");
-var btns = btnContainer.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
-}
-  
