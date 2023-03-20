629c3db30a31c1af2d79e8631a1b00452ee11a37
bhataktaBhai <naman.105.mishra@gmail.com>
2023-03-20 19:01:20 +0530

    added multiple columns

--- js/learn.js
+++ js/learn.js
@@ -1,8 +1,31 @@
 var difficulty_filter = ['hard', 'medium', 'easy'];
 var topic_filter = ['Arrays', 'Greedy'];
 
-document.querySelectorAll(".filter button").forEach(function (btn) {
+function layCards(cards) {
+    document.getElementById("container").innerHTML = "";
+    let card_containers = [];
+    let heights = [];
+    for (let i = 0; i < 5; i++) {
+        const card_container = document.createElement("div");
+        card_container.className = "card_container";
+        // card_container.id = "container" + i;
+        card_containers.push(card_container);
+        heights.push(0);
+    }
+    // get width of the window, not the entire screen
+    let NUM_CONTAINERS = Math.min(Math.floor(window.innerWidth / 300), 5);
+    for (let i = 0; i < NUM_CONTAINERS; i++) {
+        document.getElementById("container").appendChild(card_containers[i]);
+    }
+    cards.forEach(function(card){
+        const min_height = Math.min(...heights.slice(0, NUM_CONTAINERS));
+        const min_height_index = heights.indexOf(min_height);
+        card_containers[min_height_index].appendChild(card);
+        heights[min_height_index] += card.clientHeight;
+    });
+}
 
+document.querySelectorAll(".filter button").forEach(function (btn) {
     btn.addEventListener("click", function (event) {
         let type = this.parentElement.getAttribute('data-type');
 
@@ -17,6 +40,7 @@ document.querySelectorAll(".filter button").forEach(function (btn) {
     });
 });
 
+var cards = [];
 fetch(
     `https://api.airtable.com/v0/appHwUzo4ARCQQlwr/Learn?maxRecords=1000&view=Grid%20view`,
     {
@@ -36,17 +60,24 @@ fetch(
         row.fields.Topics.forEach(function(topic){
             topicsString += `<span class="topicTag">${topic}</span>`;
         });
-        document.querySelector("#container").innerHTML += `<div class="card">
-            <h2>${row.fields.Name}</h2>
-            <span class="difficultyTag ${row.fields.Difficulty}">${row.fields.Difficulty}</span>
-            <span class="topicTag">${topicsString}</span>
-            <a href="${row.fields.Link}" target="_blank">Open Problem</a>
-        </div>`;
+
+        const card = document.createElement("div");
+        card.className = "card";
+        card.innerHTML += `<h2>${row.fields.Name}</h2>`;
+        card.innerHTML += `<span class="difficultyTag ${row.fields.Difficulty}">${row.fields.Difficulty}</span>`;
+        card.innerHTML += `<span class="topicTag">${topicsString}</span>`;
+        card.innerHTML += `<a href="${row.fields.Link}" target="_blank">Open Problem</a>`;
+        cards.push(card);
+        // document.getElementById("container").appendChild(card);
     });
+})
+.then(() => {
+    layCards(cards);
 });
-// window.addEventListener('resize', function(event) {
-//     document.querySelectorAll(".card").forEach(function(card){
-//         card.style.top = card.nth-child.max_height + "px";
-//     });
-// }, true);
+
+window.addEventListener('resize', function(event) {
+    layCards(cards);
+}, true);
+
+function filter_topic()
 
