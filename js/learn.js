46cf78bb1e51ba5900285406454aae1846cde6d6
YUKTA-S <yukta.subramanian@gmail.com>
2023-03-21 18:24:13 +0530

    learn2

--- js/learn.js
+++ js/learn.js
@@ -1,15 +1,38 @@
 var difficulty_filter = ['hard', 'medium', 'easy'];
-var topic_filter = ['Arrays', 'Greedy'];
+var topic_filter = {Arrays, Strings}
+
+function layCards(cards) {
+    document.getElementById("container").innerHTML = "";
+    let card_containers = [];
+    let heights = [];
+    for (let i = 0; i < 5; i++) {
+        const card_container = document.createElement("div");
+        card_container.className = "card_container";
+        // card_container.id = "container" + i;
+        card_containers.push(card_container);
+        heights.push(0);
+    }
+    // get width of the window, not the entire screen
+    let NUM_CONTAINERS = Math.min(Math.floor(window.innerWidth / 300), 5);
+    for (let i = 0; i < NUM_CONTAINERS; i++) {
+        document.getElementById("container").appendChild(card_containers[i]);
+    }
+    cards.forEach(function(card){
+        const min_height = Math.min(...heights.slice(0, NUM_CONTAINERS));
+        const min_height_index = heights.indexOf(min_height);
+        card_containers[min_height_index].appendChild(card);
+        heights[min_height_index] += card.clientHeight;
+    });
+}
 
 document.querySelectorAll(".filter button").forEach(function (btn) {
-
     btn.addEventListener("click", function (event) {
         let type = this.parentElement.getAttribute('data-type');
 
         if (this.classList.contains("active")) {
             this.classList.remove("active");
             window[type + "_filter"].splice(window[type + "_filter"].indexOf(this.getAttribute('data')), 1)
-
+            
         } else {
             this.classList.add("active");
             window[type + "_filter"].push(this.getAttribute('data'));
@@ -17,6 +40,7 @@ document.querySelectorAll(".filter button").forEach(function (btn) {
     });
 });
 
+var cards = [];
 fetch(
     `https://api.airtable.com/v0/appHwUzo4ARCQQlwr/Learn?maxRecords=1000&view=Grid%20view`,
     {
@@ -36,17 +60,29 @@ fetch(
         row.fields.Topics.forEach(function(topic){
             topicsString += `<span class="topicTag">${topic}</span>`;
         });
-        document.querySelector("#container").innerHTML += `<div class="card">
-            <h2>${row.fields.Name}</h2>
-            <span class="difficultyTag ${row.fields.Difficulty}">${row.fields.Difficulty}</span>
-            <span class="topicTag">${topicsString}</span>
-            <a href="${row.fields.Link}" target="_blank">Open Problem</a>
-        </div>`;
-    });
-});
-// window.addEventListener('resize', function(event) {
-//     document.querySelectorAll(".card").forEach(function(card){
-//         card.style.top = card.nth-child.max_height + "px";
-//     });
-// }, true);
 
+        const card = document.createElement("div");
+        card.className = "card";
+        card.innerHTML += `<h2>${row.fields.Name}</h2>`;
+        card.innerHTML += `<span class="difficultyTag ${row.fields.Difficulty}">${row.fields.Difficulty}</span>`;
+        card.innerHTML += `<span class="topicTag">${topicsString}</span>`;
+        card.innerHTML += `<a href="${row.fields.Link}" target="_blank">Open Problem</a>`;
+        cards.push(card);
+        // document.getElementById("container").appendChild(card);
+    });
+})
+.then(() => {
+    layCards(cards);
+
+document.querySelector("#loader").classList.add("fadeout");
+        setTimeout(() => {
+            document.querySelector("#loader").style.display = "none";
+            document.querySelector("#loader").classList.remove("fadeout");
+        }, 300);
+    });
+    
+window.addEventListener('resize', function(event) {
+    layCards(cards);
+}, true);
+
+// function filter_topic()
